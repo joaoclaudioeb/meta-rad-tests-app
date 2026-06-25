828f0a67b286ce94e6b74ef5adab4fc752093cb3
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2026-06-25 20:40:53 -0300

    dac: Add implementation for DAC class

--- include/rad-tests-app/dac.hpp
+++ include/rad-tests-app/dac.hpp
@@ -7,20 +7,21 @@
 
 class DAC {
   public:
-    DAC(std::string spi_path, std::string gpio_ctrl, std::uint8_t rst_pin,
-        std::uint8_t ldac_pin);
-    int get_int_reference();
-    int set_int_reference(dac81408_ref_state_t state);
-    int get_channel_range(std::uint8_t ch);
-    int set_channel_range(std::uint8_t ch, dac81408_range_t range);
-    int set_channel_state(std::uint8_t ch, bool enabled);
-    int set_channel_voltage(std::uint8_t ch, double out_voltage);
-    bool is_channel_enabled(std::uint8_t ch);
+    DAC(std::string spiPath, std::string gpiochipName, std::uint8_t rstPin,
+        std::uint8_t ldacPin);
+    int getIntReference();
+    int setIntReference(dac81408_ref_state_t state);
+    int getChannelRange(std::uint8_t ch);
+    int setChannelRange(std::uint8_t ch, dac81408_range_t range);
+    int setChannelState(std::uint8_t ch, bool enabled);
+    int setChannelVoltage(std::uint8_t ch, double outVoltage);
+    bool isChannelEnabled(std::uint8_t ch);
+    const char *spidev() const { return spidevPath_.data(); }
 
   private:
     std::unique_ptr<dac81408_t> impl_;
-    std::string spidev_path_;
-    std::string gpiochip_name_;
+    std::string spidevPath_;
+    std::string gpiochipName_;
 };
 
 #endif
